3dcf91010faf7e79a6f817e9e15411199b0ddef7
Kieran Cormack <kcormack91@gmail.com>
2014-09-30 19:05:21 +0000

    firstmangoecomplete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -23,8 +23,13 @@ class Movie < ActiveRecord::Base
   validate :release_date_is_in_the_future
 
   def review_average
-    reviews.sum(:rating_out_of_ten)/reviews.size
+    if reviews.size == 0
+      return "0"
+    else
+      reviews.sum(:rating_out_of_ten) / reviews.size
+    end
   end
+
 
   protected
 
@@ -33,5 +38,7 @@ class Movie < ActiveRecord::Base
       errors.add(:release_date, "should probably be in the future") if release_date < Date.today
     end
   end
+
+
 
 end
